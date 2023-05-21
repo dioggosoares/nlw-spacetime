1aaa7aed85a079deeae8c71b592ca2eb7867467e
Diogo Soares <dioggosoares35@gmail.com>
2023-05-21 01:11:07 -0300

    first commit

---
+++ web/src/app/page.tsx
@@ -0,0 +1,78 @@
+import { User } from 'lucide-react'
+
+import logo from '../assets/logo.svg'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <section
+        className="relative flex flex-col items-start
+        justify-between overflow-hidden border-r border-white/10
+        bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16"
+      >
+        {/* Blur */}
+        <div
+          className="absolute right-0 top-1/2 h-[288px] w-[526px]
+          -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700
+          opacity-50 blur-full"
+        />
+        {/* Stripes */}
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+        {/* Sign In */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div
+            className="flex h-10 w-10 items-center justify-center
+            rounded-full bg-gray-400"
+          >
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
+
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={logo} alt="NLW Spacetime" />
+
+          <article className="max-w-[420px] space-y-1">
+            <h1 className="mt-5 text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="mt-1 text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </article>
+
+          <a href="" className="inline-block uppercase">
+            Cadastrar Lembrança
+          </a>
+        </div>
+      </section>
+
+      {/* Right */}
+      <section
+        className="flex flex-col bg-[url(../assets/bg-stars.svg)]
+        bg-cover p-16"
+      >
+        <article className="flex flex-1 items-center justify-center">
+          <p className="w-[22.5rem] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora!
+            </a>
+          </p>
+        </article>
+      </section>
+    </main>
+  )
+}
